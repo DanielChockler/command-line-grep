d7b9da8db1ce0199e12a331a174591168f23d9d1
Daniel Chockler <daniel.chockler@gmail.com>
2025-10-23 09:00:18 +0100

    Refactered Code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Unable to read file");
+
+    println!("{}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path : String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
